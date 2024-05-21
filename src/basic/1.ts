b51cc8e9c149b942df5d37111d8348838de22e43
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2024-05-21 16:36:26 +0300

    init

---
+++ src/basic/1.ts
@@ -0,0 +1,8 @@
+let age: number = 50;
+let username: string = "Max";
+let toggle: boolean = true;
+let empty: null = null;
+const callback = (a: number) => {
+  return 100 + a;
+};
+console.log(callback(10));
