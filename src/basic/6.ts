a6cc5f0f5532f723e550985a73fb0709de8d0917
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2024-05-22 09:48:32 +0300

    tasks 1-4 generic

--- src/basic/6.ts
+++ src/basic/6.ts
@@ -1,3 +1,13 @@
+interface User {
+  name: string;
+  age: number;
+  email: string;
+  address?: {
+    city?: string;
+    country?: string;
+  };
+}
+
 export const mango: User = {
   name: "Mango",
   age: 30,
@@ -13,13 +23,3 @@ export const poly: User = {
   age: 30,
   email: "john@example.com",
 };
-
-interface User {
-  name: string;
-  age: number;
-  email: string;
-  address?: {
-    city?: string;
-    country?: string;
-  };
-}
---
+++ src/generics/4.ts
@@ -0,0 +1,16 @@
+type User = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+};
+
+function createOrUpdateUser(initialValues: Partial<User>) {
+  email: initialValues.email;
+  password: initialValues.password;
+}
+
+createOrUpdateUser({
+  email: "user@mail.com",
+  password: "password123",
+});
